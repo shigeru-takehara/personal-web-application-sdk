8ce4f22c93085801a702c6cd5f7f8890ce35e7e6
Shigeru Takehara <shigeru.takehara@cgifederal.com>
2020-05-02 22:18:42 -0400

    Update Process.js saveData.js saveDataAsync.js

--- http-server/SERVER-SCRIPTS/jjs/util/saveData.js
+++ http-server/SERVER-SCRIPTS/jjs/util/saveData.js
@@ -1,4 +1,15 @@
 function jjsExec(args) {
+	var params = pwa.process.getArgs4Jjs(args);
+	var file = File.createTempFile("saveData", null);
+    file.deleteOnExit();
+	var writer = new FileWriter(file);
+	writer.write(params[1]);
+	writer.close();
+	
+    params[0] = encodeURI(params[0]);
+    params[1] = encodeURI(file.getAbsolutePath());
+	args = params[0] + '&' + params[1];
+
 	return pwa.process.runAsync("SERVER-SCRIPTS/js/saveDataAsync.js", args);
 }
 
